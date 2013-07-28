56ff7d58add5259c52689112c37ad9de55efc3d0
fbmnds <fbmnds7@gmail.com>
2013-07-28 15:03:52 +0200

    use en_US for consistent number formats

--- test/gini_test.clj
+++ test/gini_test.clj
@@ -2,6 +2,18 @@
   (:use midje.sweet)
   (:require [gini :refer :all]))
 
+;; save settings
+;;
+(def print-length *print-length*)
+(set! *print-length* 5)
+;;
+;; set consistent en_US number formats for number/string conversions
+;;
+(def en_US (java.util.Locale. "en" "US"))
+(def locale (java.util.Locale/getDefault))
+(if-not (= locale en_US)
+  (java.util.Locale/setDefault en_US))
+
 
 (defmacro pure-time
   "Like clojure.core/time, returns the time as a value
@@ -12,13 +24,19 @@
        (prn ~expr)
        (/ (double (- (. System (nanoTime)) start#)) 1000000.0))))
 
-(defn perf? [tn]
-  (let [[t100 t1000 t10000 t100000] tn]
-    (and (< (/ t100000 t10000) 13)
-         (< (/ t10000 t1000) 13)
-         (< (/ t1000 t100) 13))))
 
 (def tn [100 1000 10000 100000])
+(def theta 12)
+
+(defn- perf? [tn]
+  (let [[t100 t1000 t10000 t100000] tn
+        res (and (< (/ t100000 t10000) theta)
+                 (< (/ t10000 t1000) theta)
+                 (< (/ t1000 t100) theta))]
+    (if-not res
+      (println "\n- observed times:\n  ---------------\n " tn)
+      true)))
+
 
 
 ;; testing private fn:
@@ -28,7 +46,7 @@
 
 
 (defn- cum-fn-perf [n]
-  (pure-time (last (cum-fn (range n) +))))
+  (pure-time (take 5 (cum-fn (range n) +))))
 
 (fact "cum-fn"
       (cum-fn (repeat 10 1) +) => '(1 2 3 4 5 6 7 8 9 10)
@@ -44,7 +62,7 @@
 
 
 (defn- set-xy-perf [n]
-  (pure-time (last
+  (pure-time (take 5
               (@#'gini/set-xy (vec (take n (repeatedly #(rand-int 42))))
                               (vec (take n (repeatedly #(rand-int 42))))))))
 
@@ -65,7 +83,7 @@
                                  (vec (take 100000 (repeatedly #(rand-int 42))))))
 
 (defn- split-xy-perf [n]
-  (pure-time (last
+  (pure-time (take 5
               (@#'gini/split-xy (take n test-set-xy)))))
 
 (fact "split-xy"
@@ -73,22 +91,29 @@
 
 
 
-(def test-cum+-xy (vec (@#'gini/cum-fn (sort-by first > test-set-xy) @#'gini/vec+)))
+(def test-cum+-xy
+  (vec (@#'gini/cum-fn (sort-by first > test-set-xy) @#'gini/vec+)))
 
 (defn- norm-xy-perf [n]
-  (pure-time (last
+  (pure-time (take 5
               (@#'gini/norm-xy (take n test-cum+-xy)))))
 
 (fact "norm-xy"
       (perf? (map norm-xy-perf tn)) => truthy)
 
 
-
 (defn- round-zz [zz n]
   (map #(map (fn [x] (with-precision n (float x))) %) zz))
 
+(defn- format-x [x n]
+  (read-string (format (clojure.string/join ["%." (str n) "f"]) (float x))))
+
 (defn- format-zz [zz n]
-  (map #(map (fn [x] (format "%.2f" (float x))) %) zz))
+  (map #(map (fn [x] (format-x x n)) %) zz))
+
+
+(defn- x-y-perf [n]
+  (pure-time (format-zz (take 2 (@#'gini/x-y (take n test-set-xy))) 2)))
 
 (facts "x-y"
        (let [xy-1 '([0.0224 1] [0.0276 1] [0.0402 1] [0.0498 1] [0.06 1]
@@ -98,27 +123,37 @@
              xy-3 [[0.0224 0] [0.0276 1] [0.0402 2] [0.0498 3] [0.06 7]
                    [0.09 6] [0.11 5] [0.15 4] [0.19 9] [0.4 10]]]
          (fact (format-zz (@#'gini/x-y xy-1) 2)
-               => '(("0,00" "26,00" "45,00" "60,00" "71,00"
-                     "80,00" "86,00" "90,98" "95,00" "97,76" "100,00")
-                    ("0,00" "10,00" "20,00" "30,00" "40,00"
-                     "50,00" "60,00" "70,00" "80,00" "90,00" "100,00")))
+               => '((0.00 26.00 45.00 60.00 71.00
+                     80.00 86.00 90.98 95.00 97.76 100.00)
+                    (0.00 10.00 20.00 30.00 40.00
+                     50.00 60.00 70.00 80.00 90.00 100.00)))
          (fact (format-zz (@#'gini/x-y xy-2) 2)
-               => '(("0,00" "26,00" "45,00" "60,00" "71,00"
-                     "80,00" "86,00" "90,98" "95,00" "97,76" "100,00")
-                    ("0,00" "55,56" "61,11" "66,67" "72,22"
-                     "77,78" "83,33" "88,89" "94,44" "100,00" "100,00")))
+               => '((0.00 26.00 45.00 60.00 71.00
+                     80.00 86.00 90.98 95.00 97.76 100.00)
+                    (0.00 55.56 61.11 66.67 72.22
+                     77.78 83.33 88.89 94.44 100.00 100.00)))
          (fact (format-zz (@#'gini/x-y xy-3) 2)
-               => '(("0,00" "35,09" "51,75" "64,91" "74,56"
-                     "82,46" "87,72" "92,09" "95,61" "98,04" "100,00")
-                    ("0,00" "21,28" "40,43" "48,94" "59,57"
-                     "72,34" "87,23" "93,62" "97,87" "100,00" "100,00")))
+               => '((0.00 35.09 51.75 64.91 74.56
+                     82.46 87.72 92.09 95.61 98.04 100.00)
+                    (0.00 21.28 40.43 48.94 59.57
+                     72.34 87.23 93.62 97.87 100.00 100.00)))
          (fact (format-zz (@#'gini/x-y xy-3 :order-pos second) 2)
-               => '(("0,00" "35,09" "51,75" "57,02" "64,91"
-                     "74,56" "87,72" "92,09" "95,61" "98,04" "100,00")
-                    ("0,00" "21,28" "40,43" "55,32" "68,09"
-                     "78,72" "87,23" "93,62" "97,87" "100,00" "100,00")))
+               => '((0.00 35.09 51.75 57.02 64.91
+                     74.56 87.72 92.09 95.61 98.04 100.00)
+                    (0.00 21.28 40.43 55.32 68.09
+                     78.72 87.23 93.62 97.87 100.00 100.00)))
          (fact (format-zz (@#'gini/x-y xy-3 :order < :order-pos second) 2)
-               => '(("0,00" "1,96" "4,39" "7,91" "12,28"
-                     "25,44" "35,09" "42,98" "48,25" "64,91" "100,00")
-                    ("0,00" "0,00" "2,13" "6,38" "12,77"
-                     "21,28" "31,91" "44,68" "59,57" "78,72" "100,00")))))
+               => '((0.00 1.96 4.39 7.91 12.28
+                     25.44 35.09 42.98 48.25 64.91 100.00)
+                    (0.00 0.00 2.13 6.38 12.77
+                     21.28 31.91 44.68 59.57 78.72 100.00))))
+       (fact (perf? (map x-y-perf tn)) => truthy))
+
+
+
+;; restore settings
+;;
+(set! *print-length* print-length)
+;;
+(if-not (= locale en_US)
+  (java.util.Locale/setDefault locale))
