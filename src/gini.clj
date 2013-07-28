3de9cee84862f6d50c1c15e25bff16527ffaad94
fbmnds <fbmnds7@gmail.com>
2013-07-28 19:51:35 +0200

    gini-coeff prep.ed, tests OK

--- src/gini.clj
+++ src/gini.clj
@@ -21,7 +21,7 @@
 
 (defn set-xy
   ([x]
-     (set-xy x (repeat (inc (count x)) 1)))
+     (set-xy x (repeat (count x) 1)))
   ([x y]
      (map vector x y)))
 
@@ -33,11 +33,13 @@
 (defn- vec+ [x y]
   [(+ (x 0) (y 0)) (+ (x 1) (y 1))])
 
-
+;; xy must be sorted by >
+;; use norm-xy ONLY after cum-fn!
+;;
 (defn- norm-xy [xy]
   (let [max-xy (last xy)]
-    (map (fn [[x y]] [(* (/ x (max-xy 0)) 100.0)
-                     (* (/ y (max-xy 1)) 100.0)]) xy)))
+    (map (fn [[x y]] [(* 1.0 (/ x (max-xy 0)))
+                     (* 1.0 (/ y (max-xy 1)))]) xy)))
 
 
 (defn gini-xy [xy & {:keys [order order-pos]
@@ -50,6 +52,42 @@
                   :or {order >
                        order-pos first}}]
   (split-xy (gini-xy xy :order order :order-pos order-pos)))
+
+
+(defn gini-x-y-100 [xy & {:keys [order order-pos]
+                  :or {order >
+                       order-pos first}}]
+  (map #(map (partial * 100.0) %)
+       (gini-x-y xy :order order :order-pos order-pos)))
+
+(defn- x0 [z] (first (first z)))
+(defn- x1 [z] (second (first z)))
+(defn- y0 [z] (first (second z)))
+(defn- y1 [z] (second (second z)))
+(defn- s [z] (* (- (x1 z) (x0 z)) (+ (y1 z) (y0 z))))
+
+(defn gini-coeff
+  ([x]
+     (gini-coeff x (repeat (count x) 1)))
+  ([x y]
+     0))
+
+;; (def t (repeat 10 1))
+;; (def u (gini-x-y (set-xy t)))
+;; (def v (map (partial partition 2 1) u))
+;; (def w (set-xy (first v) (second v)))
+;; (reduce + (map s w))
+
+
+;; (set-xy x y)
+;; (gini-xy -%)
+;; (map #(vec (partition 2 1 %)) -%)
+;; (reduce + (map (fn [z] (* (- (x1 z) (x0 z)) (+ (y1 z) (y0 z)))) -%))
+
+;; (- 1 (last
+;; (cum-fn (map vector (map (partial partition 2 1) (gini-x-y (set-xy x y))))
+;; (fn [z] (* (- ((z 0) 1) ((z 0) 0))
+;;            (+ ((z 1) 1) ((z 1) 0)))))))))
 
 
 (defn lorenz-curve
